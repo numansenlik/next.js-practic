a3cd181fbd30dcec1892c967063c6de2b38cb468
Numan Şenlik <nmnsnlk@gmail.com>
2024-01-04 23:57:33 +0300

    Link

---
+++ nextjs/app/about/page.jsx
@@ -0,0 +1,6 @@
+function Page() {
+  return (
+    <div>About</div>
+  )
+}
+export default Page
--- nextjs/app/page.js
+++ nextjs/app/page.js
@@ -1,8 +1,20 @@
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <div >
-      Page
-    </div>
+    <>
+      <div >
+        Page
+      </div>
+      <Link href="/about">Yönlendirme1</Link>
+      <Link href="/about?name=numan">Yönlendirme2</Link>
+      <Link href={{
+      pathname:"/about",
+      query : {
+      name:"numan",
+      surname: "senlik"
+      }
+      }}>Yönlendirme3</Link>
+    </>
   )
 }
