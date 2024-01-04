f45ec9756789be681e1edb2b0955a4c01ce09b54
Numan Şenlik <nmnsnlk@gmail.com>
2024-01-05 01:27:04 +0300

    images

--- nextjs/app/about/page.jsx
+++ nextjs/app/about/page.jsx
@@ -1,6 +1,17 @@
+import Image from "next/image"
+import Link from "next/link"
+
 function Page() {
-  return (
-    <div>About</div>
-  )
+    return (
+        <div>About
+            <Link href={"/"}> AnaSayfa</Link>
+            <Image
+                src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+                alt="Picture of the author"
+                width={300}
+                height={300}
+            />
+        </div>
+    )
 }
 export default Page
--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'www.google.com',
+            port: '',
+            pathname: '/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+          },
+        ],
+      },
+}
 
 module.exports = nextConfig
